9356f3f7133731c1d0479ae4626b22bdfe6c8ac4
Matheus Szoke <84111080+MathSzoke@users.noreply.github.com>
2026-01-04 21:56:38 -0300

    [feat] Title and subtitle now is aligned on center.

--- src/di_playground.web/src/components/Header.jsx
+++ src/di_playground.web/src/components/Header.jsx
@@ -22,14 +22,15 @@ export default function Header() {
 
     return (
         <div className={s.root}>
-            <Text as="h1" size={900} weight="semibold">
+<div>
+            <Text align="center" as="h1" size={900} weight="semibold">
                 Dependency Injection
             </Text>
 
-            <Text as="h2" size={500} className={s.subtitle}>
+            <Text align="center" as="h2" size={500} className={s.subtitle}>
                 with visual feedback
             </Text>
-
+</div>
             <Text as="p" size={400} className={s.description}>
                 Dependency Injection (DI) is a design pattern that removes the responsibility of creating dependencies from a class and delegates it to an external container.
                 Instead of tightly coupling objects together, DI allows dependencies to be provided at runtime, improving testability, flexibility, and maintainability.
